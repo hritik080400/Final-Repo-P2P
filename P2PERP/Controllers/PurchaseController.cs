6ed0d19618ae38f7d7257aa690e5b7adba43ebad
Omkar_Kumbhar02 <kumbharomkar4041@gmail.com>
2025-11-11 19:26:57 +0530

    Update Email By Omkar

--- P2PERP/Controllers/PurchaseController.cs
+++ P2PERP/Controllers/PurchaseController.cs
@@ -2530,9 +2530,10 @@ namespace P2PERP.Controllers
                 using (MailMessage mail = new MailMessage())
                 {
                     //mail.From = new MailAddress("sandeshjatti5329@gmail.com", "Rahitech IT Solution");
-                    mail.From = new MailAddress("gstprocurmenterp@gmail.com", "Procurement System");
+                   // mail.From = new MailAddress("gstprocurmenterp@gmail.com", "Procurement System");
+                    mail.From = new MailAddress(WebConfigurationManager.AppSettings["MainEmail"], "Procurement System");
                     mail.To.Add(vendorEmail);
-                   // mail.To.Add("kumbharomkar765@gmail.com"); // Test email
+                   // mail.To.Add("kumbharomkar765@gmail.com"w); // Test email
                     mail.Subject = $"Purchase Order - {POCode}";
                     mail.Body = $"Dear {vendorName},\n\nPlease find attached the Purchase Order #{POCode}.\n\nThank you.\n\nRegards,\nYour Company";
                     mail.IsBodyHtml = false;
@@ -2542,7 +2543,8 @@ namespace P2PERP.Controllers
 
                     using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
                     {
-                        smtp.Credentials = new NetworkCredential("gstprocurmenterp@gmail.com", "lhrlntigzidizmju");
+                       // smtp.Credentials = new NetworkCredential("gstprocurmenterp@gmail.com", "lhrlntigzidizmju");
+                        smtp.Credentials = new NetworkCredential(WebConfigurationManager.AppSettings["MainEmail"], WebConfigurationManager.AppSettings["AppPassword"]);
                         smtp.EnableSsl = true;
                         await smtp.SendMailAsync(mail);
                     }
